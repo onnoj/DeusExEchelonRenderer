eae5b92cf87c11b008a2fc5a1a6ffd5aa7f682a3
onnoj <stuff+git@onnoj.net>
2024-04-18 20:08:46 +0200

    Improved initial cmake helper script a bit, to look for an DEUSEXFOLDER environment variable.

--- BUILD_WIN32/build.bat
+++ BUILD_WIN32/build.bat
@@ -1,30 +1,50 @@
+setlocal enabledelayedexpansion
+
 @cmake --version
 @cd /D "%~dp0"
 @rd /s /q CMAKE
 @mkdir CMAKE
 @cd CMAKE
 
-@IF EXIST "c:\games\Steam\steamapps\common\Deus Ex" @(
-	set DEUSEXFOLDER="c:\games\Steam\steamapps\common\Deus Ex"
+@IF EXIST "%DEUSEXFOLDER%\System" @(
+	@echo "Found Deus Ex at %DEUSEXFOLDER%"
+) ELSE IF EXIST "c:\games\Steam\steamapps\common\Deus Ex\System" @(
+	@set DEUSEXFOLDER="c:\games\Steam\steamapps\common\Deus Ex\System"
+	@echo Environment variable DEUSEXFOLDER not set, detected at: !DEUSEXFOLDER!
+) ELSE IF EXIST "C:\Program Files (x86)\Steam\steamapps\common\Deus Ex\System" @(
+	@set DEUSEXFOLDER="C:\Program Files (x86)\Steam\steamapps\common\Deus Ex\System"
+	@echo Environment variable DEUSEXFOLDER not set, detected at: !DEUSEXFOLDER!
+) ELSE IF EXIST "D:\Games\Steam\steamapps\common\Deus Ex" @(
+	@set DEUSEXFOLDER="D:\Games\Steam\steamapps\common\Deus Ex\System"
+	@echo Environment variable DEUSEXFOLDER not set, detected at: !DEUSEXFOLDER!
+) ELSE IF EXIST "F:\Games\Steam\steamapps\common\Deus Ex\System" @(
+	@set DEUSEXFOLDER="F:\Games\Steam\steamapps\common\Deus Ex\System"
+	@echo Environment variable DEUSEXFOLDER not set, detected at: !DEUSEXFOLDER!
+) ELSE @(
+	@echo Environment Variable DEUSEXFOLDER is either not set, or is invalid.
+	@exit 1
 )
+@set "DEUSEXFOLDER=!DEUSEXFOLDER:"=!"
 
-@IF EXIST "C:\Program Files (x86)\Steam\steamapps\common\Deus Ex\" @(
-	set DEUSEXFOLDER="C:\Program Files (x86)\Steam\steamapps\common\Deus Ex"
-)
-
-@IF EXIST "D:\Games\Steam\steamapps\common\Deus Ex" @(
-	set DEUSEXFOLDER="D:\Games\Steam\steamapps\common\Deus Ex"
-)
-
-@IF EXIST "F:\Games\Steam\steamapps\common\Deus Ex\System" @(
-	set DEUSEXFOLDER="F:\Games\Steam\steamapps\common\Deus Ex\System"
-)
-
-@cmake -DEE_DEUSEXFOLDER=%DEUSEXFOLDER%  ^
+@cmake -DEE_DEUSEXFOLDER="!DEUSEXFOLDER!"  ^
       -G"Visual Studio 17 2022" -A win32 ..\..
 
 @cd ..
 @del /Q DeusExEchelonRenderer.sln 2>/nul
 @mklink DeusExEchelonRenderer.sln "%CD%\CMAKE\DeusExEchelonRenderer.sln" 2>/nul
 
+@REM Prompt the user for symlink installation
+@echo.
+@set /p INSTALL_SYMLINK=^ ^> Do you want to install a symlink to the renderer in the Deus Ex system folder? [Y/N]^ 
+
+@REM Check user's choice
+@if /i "%INSTALL_SYMLINK%"=="Y" (
+	@del /F "!DEUSEXFOLDER!\DeusExEchelonRenderer.dll"
+    @mklink "!DEUSEXFOLDER!\DeusExEchelonRenderer.dll" "%CD%\..\BIN\USER\DeusExEchelonRenderer.dll" && @(
+	    @echo.
+		@echo Symlink created successfully.
+		@echo You'll still want to build the 'INSTALL' target to also copy over the config files, at least once.
+		@echo.
+	)
+)
 @pause

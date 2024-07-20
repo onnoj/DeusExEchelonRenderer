5d824e698cdc02894443ae0e914ef962af22d4e9
onnoj <stuff+git@onnoj.net>
2024-07-20 15:41:29 +0200

    Added various vector conversion helpers.

--- Source/Renderer/utils/utils.h
+++ Source/Renderer/utils/utils.h
@@ -1,2 +1,31 @@
 #pragma once
 
+static D3DXVECTOR3 UEVecToD3DXVec3(const FVector& pV)
+{
+  return D3DXVECTOR3{pV.X, pV.Y, pV.Z};
+}
+
+static FVector D3DXVec3ToUEVec(const D3DXVECTOR3& pV)
+{
+  return FVector{pV.x, pV.y, pV.z};
+}
+
+static D3DVECTOR UEVecToD3DVec3(const FVector& pV)
+{
+  return D3DVECTOR{pV.X, pV.Y, pV.Z};
+}
+
+static FVector D3DVec3ToUEVec(const D3DVECTOR& pV)
+{
+  return FVector{pV.x, pV.y, pV.z};
+}
+
+static D3DVECTOR D3DVec3ToD3DXVec3(const D3DVECTOR& pV)
+{
+  return D3DVECTOR{pV.x, pV.y, pV.z};
+}
+
+static D3DVECTOR D3DXVec3ToD3DVec(const D3DXVECTOR3& pV)
+{
+  return D3DVECTOR{pV.x, pV.y, pV.z};
+}
